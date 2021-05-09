1bc2fb05d7c5be5a78db1011fbc30f8fd1b5ae68
Achiv-Gupta <70216169+Achiv-Gupta@users.noreply.github.com>
2021-05-09 13:39:13 +0530

    Create Creating Strings.cpp

---
+++ Introductory Problems/Creating Strings.cpp
@@ -0,0 +1,28 @@
+typedef long long ll;
+#define int long long
+#define fast_io ios_base::sync_with_stdio(false);cin.tie(0);cout.tie(0)
+
+void solve()
+{
+    string s;
+    cin >> s;
+    sort(s.begin(), s.end());
+    bool val = true;
+    vector<string> ans;
+    while(val){
+        ans.push_back(s);
+        val = next_permutation(s.begin(), s.end());
+    }
+    cout << ans.size() << endl;
+    for(auto i : ans){
+        cout << i << endl;
+    }
+}
+
+
+signed main()
+{
+	fast_io;
+	int t=1;
+	while(t--) solve();
+}
